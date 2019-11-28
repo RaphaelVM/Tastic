40168ae6c4d6f368d50ea76282e8f71eb0cc3f7a
Niels Linssen <niels.linssen16@gmail.com>
2019-11-28 13:57:17 +0100

    MySql package and quick test regarding connection. Default page is index.aspx.

---
+++ Tastic/Tastic/classes/database.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tastic.classes
+{
+    public class database
+    {
+        public static readonly string GeneralConnectionString = "Server=mysql.feddema.dev;" +
+            "Database=tastic;User Id=tastic;Password=Tg8VTSxqPgi616907pf21Dd5;";
+
+        public static MySqlConnection GeneralConnection = new MySqlConnection();
+
+        public static bool OpenGeneralConnection()
+        {
+            CloseGeneralConnection();
+            try
+            {
+                GeneralConnection.ConnectionString = GeneralConnectionString;
+                GeneralConnection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private static bool CloseGeneralConnection()
+        {
+            try
+            {
+                GeneralConnection.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tastic/Tastic/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tastic.classes;
+
+namespace Tastic
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (database.OpenGeneralConnection())
+            {
+                lblConn.Text = "Connection made";
+            } else
+            {
+                lblConn.Text = "Connection failed";
+            }
+        }
+    }
+}
